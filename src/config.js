6a988992eb474a39ef2372fb5abe91db39f860d5
Melissa Lafranchise <pulchrit@gmail.com>
2019-08-19 16:47:31 -0700

    Changed DB_URL to point to process.env.DATABASE_URL to use db on heroku server app

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://knit-mine-admin@localhost/knit-mine-db',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://knit-mine-admin@localhost/knit-mine-db',
     JWT_SECRET: process.env.JWT_Secret || 'change this secret'
 };
