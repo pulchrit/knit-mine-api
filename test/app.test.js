9a76b9d8aeec560ee5190dd0ac9dc368e1afeeef
Melissa Lafranchise <pulchrit@gmail.com>
2019-08-18 17:03:01 -0700

    minor changes before initial deployment

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 
 describe("App", () => {
-    it('GET / responds with status 200 containing "hello world"', () => {
+    it('GET / responds with status 200 containing boilerplate greeting', () => {
         return request(app)
             .get('/')
             .expect(200, 'hey there, boilerplate');
