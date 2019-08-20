fba41e23b6ff08a50fe7f4d81a2fd5e012ca3080
Melissa Lafranchise <pulchrit@gmail.com>
2019-08-20 09:57:11 -0700

    configured cors for now url for prod and localhost for dev

--- src/app.js
+++ src/app.js
@@ -1,40 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const projectPatternsRouter = require('./project-patterns/project-patterns-router')
+/* const stitchPatternsRouter = require('./stitch-patterns/stitch-patterns-router')
+const myProjectsRouter = require ('./my-projects/my-projects-router')
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router') */
 
-const app = express();
+const app = express()
 
-const morganFormat = NODE_ENV === 'production' 
-    ? 'tiny'
-    : 'dev';
-    
-app.use(morgan(morganFormat));
-app.use(cors());
-app.use(helmet());
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
 
-app.get("/", (req, res) => {
-    res.send("hey there, boilerplate");
-});
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
 
+app.use(helmet())
 
+app.use(authRouter)
+app.use(projectPatternsRouter)
+app.use(stitchPatternsRouter)
+app.use(myProjectsRouter)
+app.use(usersRouter)
 
-// ! Keep this as the last middleware step!!
-// Add error handler middleware, express 
-// recognizes a 4 param middleware function
-// as being for error handling.
-app.use((error, req, res, next) => {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = {error: {message: 'server error'}};
-    } else {
-        console.error(error);
-        response = {message: error.message, error}
-    }
-    res.status(500).json(response);
-});
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { error: error.message, object: error }
+  }
+  res.status(500).json(response)
+})
 
-
-module.exports = app;
+module.exports = app
--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://knit-mine-admin@localhost/knit-mine-db',
-    JWT_SECRET: process.env.JWT_Secret || 'change this secret'
+    JWT_SECRET: process.env.JWT_Secret || 'change this secret',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/'
 };
