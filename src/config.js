1ebb7be53b749f813643f25492ff26c1d71dee17
Melissa Lafranchise <pulchrit@gmail.com>
2019-09-06 09:08:54 -0700

    update config and deployment commit

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://knit-mine-admin@localhost/knit-mine-db',
     JWT_SECRET: process.env.JWT_SECRET || 'change this secret',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN //|| 'http://localhost:3000'
 };
