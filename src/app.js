90140de4c572973fb275694ae858dc32c4fd7fd0
Melissa Lafranchise <pulchrit@gmail.com>
2019-09-06 14:43:23 -0700

    Added comment explaining why this version of the server app will NOT be used

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+/* This is the version that will store file uploads to the app server itself. It
+uses accepts multipart form data, processes the file uploaded to the server using
+express-formidable middleware, and then posts that multipart form data to the database with
+a url relative to the server for later GET requests by the client side. 
+I was not able to use this for my capstone because Heroku DOES NOT STORE static files
+like file uploads in it's server instances. Instead they recommend using AWS S3 to store
+such files. */
+
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
@@ -12,6 +20,7 @@ const myProjectsRouter = require ('./my-projects/my-projects-router')
 const usersRouter = require('./users/users-router') 
 
 const app = express()
+
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'dev', {
   skip: () => NODE_ENV === 'test',
